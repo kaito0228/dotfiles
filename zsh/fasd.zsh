b669559277d13638d3d78751e19015171db07c1c
kaito0228 <aqua.timez.saiko@gmail.com>
2020-03-13 10:51:49 +0900

    Use ghq & fzf

--- zsh/fasd.zsh
+++ zsh/fasd.zsh
@@ -13,5 +13,6 @@ alias a='fasd -a' # any
 alias s='fasd -si' # show / search / select
 alias d='fasd -d' # directory
 alias f='fasd -f' # file
-alias z='fasd_cd -d' # cd, same functionality as j in autojump
+#alias z='fasd_cd -d' # cd, same functionality as j in autojump
+alias z='cd $(ghq list --full-path | fzf)'
 alias zz='fasd_cd -d -i' # interactive directory jump
